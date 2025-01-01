bf8c7d227b19bbe8930caf01ee8138ec65b8e398
Nasrah Hussein <nasrah.mohamed@zewailcity.edu.eg>
2025-01-01 01:59:45 +0100

    update app

--- app.py
+++ app.py
@@ -1,16 +1,7 @@
-from flask import Flask, request, jsonify, render_template
-from flask_sqlalchemy import SQLAlchemy
-import os
+from flask import Flask, render_template
 
-username = os.getenv('MYSQL_USERNAME')
-password = os.getenv('MYSQL_PASSWORD')
-hostname = os.getenv('MYSQL_HOST')
-database = os.getenv('MYSQL_DATABASE')
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{username}:{password}@{hostname}/{database}'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
 
 
 # Serve Frontend
