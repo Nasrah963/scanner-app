b7f7f922e4173fe780040321af62af99f80a261d
Nasrah Hussein <nasrah.mohamed@zewailcity.edu.eg>
2025-01-01 01:56:46 +0100

    update app

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask
+from flask import Flask, request, jsonify, render_template
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+username = os.getenv('MYSQL_USERNAME')
+password = os.getenv('MYSQL_PASSWORD')
+hostname = os.getenv('MYSQL_HOST')
+database = os.getenv('MYSQL_DATABASE')
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{username}:{password}@{hostname}/{database}'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
+
+# Serve Frontend
 @app.route('/')
-def home():
-    return "Hello, Azure!"
+def index():
+    return render_template('index.html')
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run()
